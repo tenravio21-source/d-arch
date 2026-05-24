9a994a2b2a83c6231a253d94bd07ad367705d62c
rio ten <ravioten05@gmail.com>
2026-05-24 20:09:15 +0530

    nv

--- omarchy/current/theme/neovim.lua
+++ omarchy/current/theme/neovim.lua
@@ -1,13 +1,12 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "tokyonight-night",
+		},
+	},
 }
